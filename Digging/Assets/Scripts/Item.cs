fefabb5f20682f826b89011015ac3630600c23f9
chamuu <whtndyd813@naver.com>
2025-05-14 13:56:11 +0900

    itemSetup

--- Digging/Assets/Scripts/Item.cs
+++ Digging/Assets/Scripts/Item.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 [CreateAssetMenu]
@@ -8,4 +9,41 @@ public class Item : ScriptableObject
     public string itemName;
     public Sprite itemImage;
     public int count = 0;
+    public bool isMineral;
+    public int rarity;
+    public int value;
+    public int duplicate_value;
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        if (!isMineral)
+        {
+            switch (rarity)
+            {
+                case 1:
+                    value = 100;
+                    duplicate_value = value / 5;
+                    break;
+                case 2:
+                    value = 200;
+                    duplicate_value = value / 5;
+                    break;
+                case 3:
+                    value = 300;
+                    duplicate_value = value / 5;
+                    break;
+                case 4:
+                    value = 400;
+                    duplicate_value = value / 5;
+                    break;
+                case 5:
+                    value = 500;
+                    duplicate_value = value / 5;
+                    break;
+            }
+        }
+
+    }
+#endif
 }
