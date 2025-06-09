a6417ab7d6e50456d042086f4af86ea288ebb94c
Studer25101 <122955694+popis1412@users.noreply.github.com>
2025-06-09 22:13:38 +0900

    25.06.09(월) 업로드
    1. 횟불 라이트 변경

--- Digging/Assets/torch.cs
+++ Digging/Assets/torch.cs
@@ -1,28 +1,40 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 
 public class torch : MonoBehaviour
 {
     [SerializeField] GameObject dropItem;
     [SerializeField] Player playerScript;
+    Light2D light2D;
+    float lightInnerRadius = 0.3f;
+    float randFloat;
 
     bool itemDropped = false;
 
     private void Awake()
     {
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        light2D = this.gameObject.GetComponent<Light2D>();
         playerScript = playerObj.GetComponent<Player>();
+        randFloat = Random.Range(0f, 1f);
     }
 
     private void Update()
     {
         CheckBelowAndDrop();
+
+        if(light2D != null)
+        {
+            float radiusPingpong1 = Mathf.PingPong(Time.time / 3 + randFloat, 0.5f);
+            light2D.pointLightInnerRadius = radiusPingpong1;
+        }
     }
 
     public void CheckBelowAndDrop()
     {
         if(itemDropped) return;
-        
+
         Vector2 origin = transform.position;
         Vector2 direction = Vector2.down;
         float distance = 0.5f;
